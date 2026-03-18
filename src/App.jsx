6f55b7b4ad1f602a475ea01b9adda670dec07d44
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-18 14:53:14 +0100

    add data recovery from all input fields

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,22 @@ function App() {
 
   function getUserData(e) {
     e.preventDefault()
-    console.log("Sono attivo");
+    console.log(data);
   }
   return (
     <>
-      <form onChange={getUserData}>
+      <form onSubmit={getUserData}>
         <label >Author: </label>
         <input type="text" id="author" name="author" value={data.author} onChange={e => setData({ ...data, author: e.target.value })} /><br /><br />
 
         <label >Title: </label>
-        <input type="text" id="title" name="title" /><br /><br />
+        <input type="text" id="title" name="title" value={data.title} onChange={e => setData({...data, title: e.target.value})}/><br /><br />
 
         <label >Body: </label>
-        <textarea type="text" id="body" name="body" /><br /><br />
+        <textarea type="text" id="body" name="body" value={data.body} onChange={e=> setData({...data, body: e.target.value})}/><br /><br />
 
         <label>Do you want to make it public?</label>
-        <input type="checkbox" id="checkbox" name="checkbox" />
+        <input type="checkbox" id="checkbox" name="checkbox" checked={data.checkbox} onChange={e => setData({...data, checkbox: e.target.checked})} />
 
         <input type="submit" value="Submit" />
       </form>
