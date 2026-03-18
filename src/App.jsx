368a80de1e42e3f4266ba19d070bdfdb69852a4c
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-18 14:41:43 +0100

    add setData on first input

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
+import { useState } from 'react';
 import './App.css'
 import axios from 'axios'
 
-
 function App() {
- 
+
+  const [data, setData] = useState({
+
+    author: "",
+    title: "",
+    body: "",
+    checkbox: false
+
+  })
+
+
+  function getUserData(e) {
+    e.preventDefault()
+    console.log("Sono attivo");
+  }
   return (
     <>
-      <form>
+      <form onChange={getUserData}>
         <label >Author: </label>
-        <input type="text" id="author" name="author" /><br/><br/>
-        
+        <input type="text" id="author" name="author" value={data.author} onChange={e => setData({ ...data, author: e.target.value })} /><br /><br />
+
         <label >Title: </label>
-        <input type="text" id="title" name="title" /><br/><br/>
+        <input type="text" id="title" name="title" /><br /><br />
 
         <label >Body: </label>
-        <textarea type="text" id="body" name="body" /><br/><br/>
+        <textarea type="text" id="body" name="body" /><br /><br />
+
         <label>Do you want to make it public?</label>
         <input type="checkbox" id="checkbox" name="checkbox" />
-        
+
         <input type="submit" value="Submit" />
       </form>
     </>
